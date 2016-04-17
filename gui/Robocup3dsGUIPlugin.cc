bc7d970b12f3db3ff7b821d0281b2d28fb802006
jasonzliang <jasonz@osrfoundation.org>
2016-04-17 14:29:53 -0700

    increased separation

--- gui/Robocup3dsGUIPlugin.cc
+++ gui/Robocup3dsGUIPlugin.cc
@@ -101,7 +101,7 @@ void Robocup3dsGUIPlugin::AddGameTimeWidget(QHBoxLayout *_frameLayout)
 /////////////////////////////////////////////////
 void Robocup3dsGUIPlugin::AddTeamWidget(QHBoxLayout *_frameLayout)
 {
-  _frameLayout->addSpacerItem(new QSpacerItem(50, 1, QSizePolicy::Fixed));
+  _frameLayout->addSpacerItem(new QSpacerItem(60, 1, QSizePolicy::Fixed));
   QLabel *teamLabel = new QLabel();
   teamLabel->setStyleSheet("QLabel {color : #99FFFF;}");
   _frameLayout->addWidget(teamLabel);
