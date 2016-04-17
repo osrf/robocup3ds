e713890b2a94a72d939a9b3605c90e6c9774c618
jasonzliang <jasonz@osrfoundation.org>
2016-04-17 04:32:28 -0700

    increased width

--- gui/Robocup3dsGUIPlugin.cc
+++ gui/Robocup3dsGUIPlugin.cc
@@ -101,7 +101,7 @@ void Robocup3dsGUIPlugin::AddGameTimeWidget(QHBoxLayout *_frameLayout)
 /////////////////////////////////////////////////
 void Robocup3dsGUIPlugin::AddTeamWidget(QHBoxLayout *_frameLayout)
 {
-  _frameLayout->addSpacerItem(new QSpacerItem(30, 1, QSizePolicy::Fixed));
+  _frameLayout->addSpacerItem(new QSpacerItem(50, 1, QSizePolicy::Fixed));
   QLabel *teamLabel = new QLabel();
   teamLabel->setStyleSheet("QLabel {color : #99FFFF;}");
   _frameLayout->addWidget(teamLabel);
